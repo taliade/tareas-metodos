5c04626a21112235f0237b109d88f09c20540192
Talia Rodrigues <taliaraodrigues@gmail.com>
2023-11-17 20:04:10 -0300

    add layout

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override')
+const fs = require('fs');
+
 const PORT = 4000;
 
 //leer mis archivos estaticos desde public
@@ -12,7 +16,15 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
+// Confi Layout 
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+
+// Rutas
 app.use(require('./src/routes/routes'));
 
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const controller = require('../controllers/tareasControllers');
 
 router.get('/', controller.index );
+router.post('/', controller.store ); //parte de administracion
+router.put('/:id', controller.update); //parte de actualizacion 
+router.delete('/:id', controller.destroy); //parte de de eliminar tachar
+
 
 module.exports = router
